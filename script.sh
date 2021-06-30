d5472023275263c12af4e530355d82efb655e8cf
aracloud <aracloud@gmx.net>
2021-06-30 16:21:09 +0200

    change

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 date > provision.txt
-# Install  tools needed
+#sudo apt install cloud-initramfs-growroot Install  tools needed
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install \
