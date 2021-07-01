efa619bdb1e0ceb6580ca561d3e2528b84d38f00
aracloud <aracloud@gmx.net>
2021-07-01 12:56:21 +0200

    change

--- script.sh
+++ script.sh
@@ -16,9 +16,15 @@ mlocate
 
 cat <<EOF > ~/hostset.sh
 #!/bin/bash
+
 # set hostname to what dhcp has configured for that MAC/IP
-myip=\$(ifconfig ens18 |grep netmask |cut -d " " -f 10)
-myhostname=\$(nslookup \$myip |grep "name ="|sed 's/^.*name = //' |cut -d "." -f 1)
-echo \$myhostname > /etc/hostname
-sed -i s/ubuntu-20-04/\$myhostname/g /etc/hosts
+# and hostset.done file does not exist (one time execution)
+if [ ! -f hostset.done ]
+    myip=\$(ifconfig ens18 |grep netmask |cut -d " " -f 10)
+    myhostname=\$(nslookup \$myip |grep "name ="|sed 's/^.*name = //' |cut -d "." -f 1)
+    echo \$myhostname > hostname
+    sudo mv hostname /etc/hostname
+    sudo sed -i s/ubuntu-20-04/\$myhostname/g /etc/hosts
+    touch hostset.done
+fi
 EOF
